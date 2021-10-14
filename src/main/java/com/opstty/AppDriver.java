056cee686fdf70d077b8281f8a8a6e45dbbeae65
Karis <75758274+KarisG@users.noreply.github.com>
2021-10-14 19:46:14 +0200

    Creation of files

--- src/main/java/com/opstty/AppDriver.java
+++ src/main/java/com/opstty/AppDriver.java
@@ -1,6 +1,6 @@
 package com.opstty;
 
-import com.opstty.job.WordCount;
+import com.opstty.job.*;
 import org.apache.hadoop.util.ProgramDriver;
 
 public class AppDriver {
@@ -11,7 +11,16 @@ public class AppDriver {
         try {
             programDriver.addClass("wordcount", WordCount.class,
                     "A map/reduce program that counts the words in the input files.");
-
+            programDriver.addClass("liste", liste.class,
+                    "A map/reduce program that counts the words in the input files.");
+            programDriver.addClass("lis_spec", lis_spec.class,
+                    "A map/reduce program that counts the words in the input files.");
+            programDriver.addClass("trees_count", trees_count.class,
+                    "A map/reduce program that counts the words in the input files.");
+            programDriver.addClass("size_job", size_job.class,
+                    "A map/reduce program that counts the words in the input files.");
+            programDriver.addClass("sort_height_job", sort_height_job.class,
+                    "A map/reduce program that counts the words in the input files.");
             exitCode = programDriver.run(argv);
         } catch (Throwable throwable) {
             throwable.printStackTrace();
